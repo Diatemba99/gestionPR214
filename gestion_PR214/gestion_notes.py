ce5b50b8eb8dcd35285e85bc6c8c53202ad85968
Martin Claude Diatta <58808297+Diatemba99@users.noreply.github.com>
2020-12-02 23:07:20 +0000

    Add files via upload
    Premiere partie du job

---
+++ gestion_PR214/gestion_notes.py
@@ -0,0 +1,45 @@
+"""
+module pour la gestion des notes
+"""
+# from pages import *
+# from gestion_modules import *
+
+
+# def ajouterNotes():
+#     afficherEtudiant()
+#     afficherModule()
+
+# ajouterNotes()
+
+
+
+#menu principale de ce module
+# def menu_gestionNote():
+#     print('1-Ajouter une note')
+#     print('2-Modifier une note')
+#     print('3-Afficher la note d’un étudiant pour un module donné')
+#     print("4-Afficher les notes d’un étudiant pour tous les module")
+#     print('5-Afficher les notes de tous les étudiants pour tous les modules')
+    
+    # while True:   
+    #     choix = int(input('faites votre choix : '))
+        # if choix == 1:
+        #     # ajouterModule()
+        # elif choix == 2:    
+        #     # modifier_module()
+        # elif choix == 3:
+        #     # rechercher_module()  
+        # elif choix == 4:
+        #     # supprimer_module()
+        # elif choix == 5:
+        #     # afficherModule()
+        # elif choix == 6:
+        #     pass
+        # else:
+        #     print("choix non pris en compte")
+# menu_gestionNote()
+
+
+
+
+
