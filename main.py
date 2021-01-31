febeb15cbfdf77d435c4dbdc722e86b764b73823
Martin Claude Diatta <58808297+Diatemba99@users.noreply.github.com>
2021-01-31 20:40:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 """
 ceci est le module principale du programme
 """
-
 import sys
-
 from view import affichage
 from menu import menuMain
 from model import menu_gestionEtudiant,menu_seance,menu_module
 
 def main():
     menuMain()
-
     
 if __name__=="__main__":
     main()
