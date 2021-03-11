cece5d23a86a2be72a5387a36cbb52c252bd8412
Martin Claude Diatta <58808297+Diatemba99@users.noreply.github.com>
2021-03-12 00:19:05 +0100

    Update gestion_notes.py

--- gestion_notes.py
+++ gestion_notes.py
@@ -16,7 +16,7 @@ module pour la gestion des notes
      print('2-Modifier une note')
      print('3-Afficher la note d’un étudiant pour un module donné')
      print("4-Afficher les notes d’un étudiant pour tous les module")
-#     print('5-Afficher les notes de tous les étudiants pour tous les modules')
+     print('5-Afficher les notes de tous les étudiants pour tous les modules')
     
     # while True:   
     #     choix = int(input('faites votre choix : '))
