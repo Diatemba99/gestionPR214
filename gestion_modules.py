529de8ac03387f13220645cee3646b307d450268
Martin Claude Diatta <58808297+Diatemba99@users.noreply.github.com>
2021-03-12 00:03:43 +0100

    Update gestion_modules.py

--- gestion_modules.py
+++ gestion_modules.py
@@ -11,7 +11,7 @@ date = now.strftime("%Y-%m-%d")
 heureActu_mat = now.strftime("%H%M%S")
 
 #creation d'une liste pour contenir les données de tout les modules
-#creation d'une
+#creation
 dico_module = []
 
 #liste pour les donnees a afficher
